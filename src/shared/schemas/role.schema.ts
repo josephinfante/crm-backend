73eda536673f2254b79ffc120237f50e1c12926e
Joseph Infante <josephinfante.y@gmail.com>
2023-11-06 00:53:21 -0500

    creating role schema for sequelize

---
+++ src/shared/schemas/role.schema.ts
@@ -0,0 +1,22 @@
+import { DataTypes } from "sequelize";
+import { database } from "../connections/database/mysql";
+
+export const Role = database.define("roles", {
+    id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+});

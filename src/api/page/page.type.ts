b661dfb06b71fcc76624d86b7def3611877d4da2
Joseph Infante <josephinfante.y@gmail.com>
2023-11-08 23:34:33 -0500

    adding missing interfaces for page

--- src/api/page/page.type.ts
+++ src/api/page/page.type.ts
@@ -1,3 +1,10 @@
+export interface CreatePageInterface {
+    name: string;
+    role: string;
+}
+
+export interface UpdatePageInterface extends CreatePageInterface {}
+
 export interface PageInterface {
     id: string;
     name: string;

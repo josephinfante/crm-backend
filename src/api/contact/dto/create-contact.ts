82b2db2abfc259ab09cf49343e9730747a10b828
Joseph Infante <josephinfante.y@gmail.com>
2023-11-09 23:21:57 -0500

    adding function to check for an existence record in the DB

--- src/api/contact/dto/create-contact.ts
+++ src/api/contact/dto/create-contact.ts
@@ -6,17 +6,7 @@ import { UniqueID } from "../../../shared/utils";
 
 export async function CreateContact(data: CreateContactInterface) {
     try {
-        const contact = await Contact.findAll({
-            where: {
-                [Op.or]: [
-                    {email: data.email},
-                    {phone_number: data.phone_number},
-                    {facebook_id: data.facebook_id},
-                    {instagram_id: data.instagram_id},
-                    {document_type: data.document_type, document_number: data.document_number},
-                ]
-            }
-        }).catch(_error => { throw new ContactError("Ha ocurrido un error al revisar el contacto.")}).then(contact => contact);
+        const contact = await checkRecordExistence(data);
         if (contact.length > 0) throw new Error("Ya existe un contacto con los datos proporcionados.");
         await Contact.create({
             id: UniqueID.generate(),
@@ -36,4 +26,35 @@ export async function CreateContact(data: CreateContactInterface) {
         if (error instanceof Error && error.message) throw new ContactError(error.message);
         else throw new Error("Ha ocurrido un error al crear el contacto.");
     }
+}
+
+async function checkRecordExistence(data: CreateContactInterface) {
+    if ((!data.document_type || !data.document_number) && !data.email && !data.facebook_id && !data.instagram_id && !data.phone_number) return [];
+    const whereCondition: {
+        [key: string]: any;
+    } = {};
+    if (data.document_type || data.document_number) {
+        (whereCondition as any)[Op.or] = (whereCondition as any)[Op.or] || [];
+        (whereCondition as any)[Op.or].push({ document_type: data.document_type, document_number: data.document_number });
+    }
+    if (data.email) {
+        (whereCondition as any)[Op.or] = (whereCondition as any)[Op.or] || [];
+        (whereCondition as any)[Op.or].push({ email: data.email });
+    }
+    if (data.facebook_id) {
+        (whereCondition as any)[Op.or] = (whereCondition as any)[Op.or] || [];
+        (whereCondition as any)[Op.or].push({ facebook_id: data.facebook_id });
+    }
+    if (data.instagram_id) {
+        (whereCondition as any)[Op.or] = (whereCondition as any)[Op.or] || [];
+        (whereCondition as any)[Op.or].push({ instagram_id: data.instagram_id });
+    }
+    if (data.phone_number) {
+        (whereCondition as any)[Op.or] = (whereCondition as any)[Op.or] || [];
+        (whereCondition as any)[Op.or].push({ phone_number: data.phone_number });
+    }
+    const record_exists = await Contact.findAll({
+            where: whereCondition
+        }).catch((_error: any) => {throw new ContactError("Ha ocurrido un error al revisar el contacto.")}).then((records: any) => records);
+    return record_exists;
 }

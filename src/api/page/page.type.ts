562fd0bb103a76c252c10d0cbf0129c5e2de93d6
Joseph Infante <josephinfante.y@gmail.com>
2023-11-14 16:25:21 -0500

    adding new types

--- src/api/page/page.type.ts
+++ src/api/page/page.type.ts
@@ -1,14 +1,21 @@
 export interface CreatePageInterface {
     name: string;
+    nickname: string;
+    internal_url: string;
+    external_url: string;
     roles: string[];
 }
 
-export interface UpdatePageInterface extends CreatePageInterface {}
+export interface UpdatePageInterface extends CreatePageInterface {
+}
 
 export interface PageInterface {
     id: string;
     name: string;
+    nickname: string;
+    internal_url: string;
+    external_url: string;
+    role_ids: string;
     createdAt: Date;
     updatedAt: Date;
-    role_id: string;
 }

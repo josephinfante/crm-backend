583d8b851b9a1a656644b3c5fb581a99585025a6
Joseph Infante <josephinfante.y@gmail.com>
2023-11-14 16:29:34 -0500

    removing middleware

--- src/api/role/role.routes.ts
+++ src/api/role/role.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { createRole, deleteRole, getAllRoles, getRole, updateRole } from "./role.controller";
-import { isAdminAuth } from "../../shared/middlewares";
+// import { isAdminAuth } from "../../shared/middlewares";
 
 export const roleRouter = Router();
 
-roleRouter.post("/v1/role", isAdminAuth, createRole);
-roleRouter.get("/v1/role/:id", isAdminAuth, getRole);
-roleRouter.put("/v1/role/:id", isAdminAuth, updateRole);
-roleRouter.delete("/v1/role/:id", isAdminAuth, deleteRole);
-roleRouter.get("/v1/roles", isAdminAuth, getAllRoles);
+roleRouter.post("/v1/role", createRole);
+roleRouter.get("/v1/role/:id", getRole);
+roleRouter.put("/v1/role/:id", updateRole);
+roleRouter.delete("/v1/role/:id", deleteRole);
+roleRouter.get("/v1/roles", getAllRoles);

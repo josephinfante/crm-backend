3985cdeae72f4e464c111d9c709e44981811ca5b
Joseph Infante <josephinfante.y@gmail.com>
2023-11-14 16:25:08 -0500

    renaming function

--- src/api/page/page.routes.ts
+++ src/api/page/page.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createPage, getPage, updatePage, deletePage, getAllPages, getPagesByRole } from "./page.controller";
+import { createPage, getPage, updatePage, deletePage, getAllPages, getPagesByRoleName } from "./page.controller";
 
 export const pageRouter = Router();
 
@@ -8,4 +8,4 @@ pageRouter.get("/v1/page/:id", getPage);
 pageRouter.put("/v1/page/:id", updatePage);
 pageRouter.delete("/v1/page/:id", deletePage);
 pageRouter.get("/v1/pages", getAllPages);
-pageRouter.get("/v1/pages/:role", getPagesByRole);
+pageRouter.get("/v1/pages/:role_name", getPagesByRoleName);

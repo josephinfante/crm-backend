ed95bbd75899c48e6338b527c9aebc4ad47188a6
Joseph Infante <josephinfante.y@gmail.com>
2023-11-14 16:30:25 -0500

    updating functions to create and update role

--- src/api/role/dto/create-role.ts
+++ src/api/role/dto/create-role.ts
@@ -1,17 +1,40 @@
-import { RoleError } from "../../../shared/errors";
-import { Role } from "../../../shared/schemas";
+import { ComponentError, PageError, RoleError } from "../../../shared/errors";
+import { Component, Page, Role } from "../../../shared/schemas";
 import { UniqueID } from "../../../shared/utils";
+import { CreatePermission } from "../../permission/dto/create-permission";
+import { CreateRoleInterface } from "../role.type";
 
-export async function CreateRole(role: {name: string}) {
+export async function CreateRole(data: CreateRoleInterface) {
     try {
-        const role_exists = await Role.findOne({where: {name: role.name}}).catch(_error => {throw new RoleError('Ha ocurrido un error al revisar el rol.')}).then(role => role);
-        if (role_exists) throw new RoleError(`El rol ${role.name} ya existe.`);
+        const role_exists = await Role.findOne({where: {name: data.name}}).catch(_error => {throw new RoleError('Ha ocurrido un error al revisar el rol.')}).then(role => role);
+        if (role_exists) throw new RoleError(`El rol ${data.name} ya existe.`);
+        const role_id = UniqueID.generate();
         await Role.create({
-            id: UniqueID.generate(),
-            name: role.name,
+            id: role_id,
+            name: data.name,
             createdAt: new Date(),
             updatedAt: new Date(),
         }).catch(_error => {throw new RoleError('Ha ocurrido un error al tratar de crear el rol.')});
+        for (const page of data.pages) {
+            const page_exist = await Page.findOne({where: {id: page.id}}).catch(_error => {throw new PageError('Ha ocurrido un error al revisar la página.')}).then(page => page);
+            if (page_exist) {
+                page_exist.set({role_ids: page_exist.dataValues.role_ids + ',' + role_id});
+                await page_exist.save().catch(_error => {throw new PageError('Ha ocurrido un error al tratar de actualizar la pagina.')});
+            }
+        }
+        for (const component of data.components) {
+            const component_exist = await Component.findOne({where: {id: component.id}}).catch(_error => {throw new ComponentError('Ha ocurrido un error al revisar el componente.')}).then(component => component);
+            if (component_exist) {
+                await CreatePermission({
+                    create: component.permissions.create,
+                    read: component.permissions.read,
+                    update: component.permissions.update,
+                    delete: component.permissions.delete,
+                    component_id: component.id,
+                    role_id: role_id,
+                })
+            }
+        }
     } catch (error) {
         if (error instanceof Error && error.message) throw new RoleError(error.message);
         else throw new Error('Ha ocurrido un error al crear el rol.');
--- src/api/role/dto/update-role.ts
+++ src/api/role/dto/update-role.ts
@@ -1,15 +1,36 @@
-import { RoleError } from "../../../shared/errors";
-import { Role } from "../../../shared/schemas";
+import { PageError, RoleError } from "../../../shared/errors";
+import { Page, Role } from "../../../shared/schemas";
+import { UpdatePermission } from "../../permission/dto/update-permission";
+import { UpdateRoleInterface } from "../role.type";
 
-export async function UpdateRole(id: string, name: string) {
+export async function UpdateRole(id: string, data: UpdateRoleInterface) {
     try {
         const role = await Role.findOne({where: {id: id}}).catch(_error => {throw new RoleError('Ha ocurrido un error al revisar el rol.')}).then(role => role);
         if (!role) throw new RoleError(`El rol con ID ${id} no existe.`);
-        role.set({
-            name: name,
-            updatedAt: new Date(),
-        });
-        await role.save().catch(_error => {throw new RoleError('Ha ocurrido un error al tratar de actualizar el rol.')});
+        if (data.name !== role.dataValues.name) {
+            await role.save().catch(_error => {throw new RoleError('Ha ocurrido un error al tratar de actualizar el rol.')});
+        }
+        if (data.pages && data.pages.length > 0) {
+            for (const page of data.pages) {
+                const page_exist = await Page.findOne({where: {id: page.id}}).catch(_error => {throw new PageError('Ha ocurrido un error al revisar la página.')}).then(page => page);
+                if (page_exist && !page_exist.dataValues.role_ids.includes(id)) {
+                    page_exist.set({role_ids: page_exist.dataValues.role_ids + ',' + id});
+                    await page_exist.save().catch(_error => {throw new PageError('Ha ocurrido un error al tratar de actualizar la pagina.')});
+                }
+            }
+        }
+        if (data.components && data.components.length > 0) {
+            for (const component of data.components) {
+                await UpdatePermission({
+                    component_id: component.id,
+                    role_id: id,
+                    create: component.permissions.create,
+                    read: component.permissions.read,
+                    update: component.permissions.update,
+                    delete: component.permissions.delete,
+                });
+            }
+        }
     } catch (error) {
         if (error instanceof Error && error.message) throw new RoleError(error.message);
         else throw new Error('Ha ocurrido un error al actualizar el rol.');

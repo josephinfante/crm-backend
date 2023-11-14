4bfeb1aea3d32a5ddb0e812405ec348403ce822b
Joseph Infante <josephinfante.y@gmail.com>
2023-11-14 16:30:56 -0500

    adding new type for user login

--- src/api/user/user.type.ts
+++ src/api/user/user.type.ts
@@ -25,3 +25,31 @@ export interface UserInterface {
     updatedAt: Date;
     role_id: string;
 }
+
+export interface LoginInterface {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    document_number: string;
+    document_type: string;
+    phone_number: string;
+    token: string;
+    pages: {
+        name: string;
+        nickname: string;
+        internal_url: string;
+        external_url: string;
+    }[];
+    permissions: {
+        component: string;
+        permissions: {
+            create: boolean;
+            read: boolean;
+            update: boolean;
+            delete: boolean;
+        };
+    }[];
+    createdAt: Date;
+    updatedAt: Date;
+}

c2ebe8a00037567dab99a3f5f6eba7b9086eef42
Joseph Infante <josephinfante.y@gmail.com>
2023-11-14 16:29:49 -0500

    adding new types when creating role

--- src/api/role/role.type.ts
+++ src/api/role/role.type.ts
@@ -1,6 +1,23 @@
-export interface RoleInterface {
-    id: string;
+export interface CreateRoleInterface {
     name: string;
+    pages: {
+        id: string;
+    }[];
+    components: {
+        id: string;
+        permissions: {
+            create: boolean;
+            read: boolean;
+            update: boolean;
+            delete: boolean;
+        };
+    }[];
+}
+
+export interface UpdateRoleInterface extends CreateRoleInterface {}
+
+export interface RoleInterface extends CreateRoleInterface {
+    id: string;
     createdAt: Date;
     updatedAt: Date;
 }

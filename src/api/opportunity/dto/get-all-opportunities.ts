891bdc1d47086f5d6c7d9b028e51e3f7a8088908
Joseph Infante <josephinfante.y@gmail.com>
2023-11-14 20:47:53 -0500

    modifying reponse of opportunities

--- src/api/opportunity/dto/get-all-opportunities.ts
+++ src/api/opportunity/dto/get-all-opportunities.ts
@@ -1,17 +1,36 @@
 import { OpportunityError } from "../../../shared/errors";
-import { Opportunity } from "../../../shared/schemas";
+import { Career, Contact, Opportunity, Semester, User } from "../../../shared/schemas";
 
 export async function GetAllOpportunities(page: number) {
     try {
         const { count, rows } = await Opportunity.findAndCountAll({
+            include: [
+                { model: Contact, attributes: ['id', 'first_name', 'last_name_1'] }, 
+                { model: Career, attributes: ['id', 'name'] }, 
+                { model: Semester, attributes: ['id', 'name'] }, 
+                { model: User, attributes: ['id', 'first_name', 'last_name'] }
+            ],
             limit: 100,
             offset: (page - 1) * 100,
-        }).catch((_error) => { throw new OpportunityError("Ha ocurrido un error al tratar de obtener las oportunidades."); }).then(opportunity => opportunity);
+        }).catch((_error) => {throw new OpportunityError("Ha ocurrido un error al tratar de obtener las oportunidades."); }).then(opportunity => opportunity);
         const reshapedOpportunities = rows.map(opportunity => ({
             id: opportunity.dataValues.id,
-            contact_id: opportunity.dataValues.contact_id,
-            career_id: opportunity.dataValues.career_id,
-            semester_id: opportunity.dataValues.semester_id,
+            contact: {
+                id: opportunity.dataValues.contact.dataValues.id,
+                name: `${opportunity.dataValues.contact.dataValues.first_name} ${opportunity.dataValues.contact.dataValues.last_name_1}`,
+            },
+            career: {
+                id: opportunity.dataValues.career.dataValues.id,
+                name: opportunity.dataValues.career.dataValues.name,
+            },
+            semester: {
+                id: opportunity.dataValues.semester.dataValues.id,
+                name: opportunity.dataValues.semester.dataValues.name,
+            },
+            user: {
+                id: opportunity.dataValues.user.dataValues.id,
+                name: `${opportunity.dataValues.user.dataValues.first_name} ${opportunity.dataValues.user.dataValues.last_name}`,
+            },
             createdAt: opportunity.dataValues.createdAt,
             updatedAt: opportunity.dataValues.updatedAt,
         }));

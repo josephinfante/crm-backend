e9571c913d57af7af6ce619592acec391971d60d
Joseph Infante <josephinfante.y@gmail.com>
2023-11-14 20:48:53 -0500

    adding user_id to opportunity schema

--- src/shared/schemas/opportunity.schema.ts
+++ src/shared/schemas/opportunity.schema.ts
@@ -3,6 +3,7 @@ import { database } from "../connections/database/mysql";
 import { Contact } from "./contact.schema";
 import { Career } from "./career.schema";
 import { Semester } from "./semester.schema";
+import { User } from "./user.schema";
 
 export const Opportunity = database.define("opportunities", {
     id: {
@@ -34,7 +35,16 @@ export const Opportunity = database.define("opportunities", {
             key: 'id',
         },
     },
+    user_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id',
+        },
+    }
 });
 Opportunity.hasOne(Contact, {foreignKey: 'id', sourceKey: 'contact_id'});
 Opportunity.hasOne(Career, {foreignKey: 'id', sourceKey: 'career_id'});
 Opportunity.hasOne(Semester, {foreignKey: 'id', sourceKey: 'semester_id'});
+Opportunity.hasOne(User, {foreignKey: 'id', sourceKey: 'user_id'});

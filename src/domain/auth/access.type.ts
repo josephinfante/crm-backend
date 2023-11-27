7865a6b938f09f496551ea9d48ff7d01c0ce0501
Joseph Infante <josephinfante.y@gmail.com>
2023-11-27 18:03:52 -0500

    adding access interface

---
+++ src/domain/auth/access.type.ts
@@ -0,0 +1,13 @@
+export interface IAccessPermission {
+    user_id: string;
+    super_admin: boolean | null;
+    role_name: string;
+    permission: {
+        create: boolean;
+        read: boolean;
+        update: boolean;
+        delete: boolean;
+        read_all: boolean;
+        read_deleted: boolean;
+    }
+}

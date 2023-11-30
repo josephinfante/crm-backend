fdae427d6b19306a56d9441cfd4dfd5fc5dd8223
Joseph Infante <josephinfante.y@gmail.com>
2023-11-29 21:40:50 -0500

    adding presenter for contact

---
+++ src/interfaces/presenters/contact-language.presenter.ts
@@ -0,0 +1,38 @@
+import { IContactLanguage } from "../../domain/contanct-language/contact-language.type";
+import { ILanguage } from "../../domain/language/language.type";
+
+export interface IContactLanguageResponse {
+    id: string;
+    native: boolean;
+    level: string;
+    speak: string;
+    read: string;
+    listen: string;
+    hidden: boolean;
+    deleted: boolean;
+    updatedAt: number;
+    createdAt: number;
+    language: {
+        id: string;
+        name: string;
+    };
+}
+
+export function ContactLanguagePresenter (contact_language: IContactLanguage, language: ILanguage): IContactLanguageResponse {
+    return {
+        id: contact_language.id,
+        native: contact_language.native,
+        level: contact_language.level,
+        speak: contact_language.speak,
+        read: contact_language.read,
+        listen: contact_language.listen,
+        hidden: contact_language.hidden,
+        deleted: contact_language.deleted,
+        updatedAt: contact_language.updatedAt,
+        createdAt: contact_language.createdAt,
+        language: {
+            id: language.id,
+            name: language.name,
+        },
+    };
+}

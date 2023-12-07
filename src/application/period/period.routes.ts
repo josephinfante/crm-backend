a57e2fa9648f5c38ad9ba195b81336db29022b04
Joseph Infante <josephinfante.y@gmail.com>
2023-12-06 19:00:20 -0500

    updating route name

--- src/application/period/period.routes.ts
+++ src/application/period/period.routes.ts
@@ -12,6 +12,6 @@ const periodController = new PeriodController(periodCrudUseCase);
 periodRouter.post("/v1/period", Access.canCreate('period'), periodController.create.bind(periodController));
 periodRouter.put("/v1/period/:id", Access.canUpdate('period'), periodController.update.bind(periodController));
 periodRouter.delete("/v1/period/:id", Access.canDelete('period'), periodController.delete.bind(periodController));
-periodRouter.get("/v1/period/:business_unit_id", Access.canRead('period'), periodController.findByBusinessUnitId.bind(periodController));
+periodRouter.get("/v1/periods/:business_unit_id", Access.canRead('period'), periodController.findByBusinessUnitId.bind(periodController));
 periodRouter.get("/v1/periods", Access.canRead('period'), periodController.findAll.bind(periodController));
 export default periodRouter;

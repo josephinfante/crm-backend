c611b40475442a603fe226efc0e926c5a311ba0d
Joseph Infante <josephinfante.y@gmail.com>
2023-12-11 21:51:37 -0500

    fixing function to return all pages to super admin

--- src/infrastructure/menu/menu.dao.ts
+++ src/infrastructure/menu/menu.dao.ts
@@ -75,25 +75,50 @@ export async function FindPageWithMenu(access:IAccessPermission, role_id: string
     }
 }
 
-export async function FindAllPagesWithRoleId(role_id: string): Promise<IRolePage[]> {
+export async function FindAllPagesWithRoleId( access: IAccessPermission, role_id: string ): Promise<IRolePage[]> {
     try {
-        const menus = await MenuModel.findAll({ 
+        const menus = access.super_admin === true ?
+            await PageModel.findAll({
+                    where: { deleted: false, hidden: false },
+                })
+                .then(pages => pages)
+                .catch(_error => {throw new MenuError(_error.message??'Ha ocurrido un error al tratar de buscar las páginas.')}) :
+            await MenuModel.findAll({ 
                     where: { role_id: role_id },
-                    include: [{ model: PageModel, attributes: ['id', 'name', 'external_url', 'createdAt', 'updatedAt'] }]
+                    include: [{ model: PageModel, attributes: ['id', 'name', 'external_url', 'createdAt', 'updatedAt'], where: { deleted: false, hidden: false } }]
                 })
                 .then(menus => menus)
-                .catch(_error => {throw new MenuError('Ha ocurrido un error al tratar de buscar los menus.')});
+                .catch(_error => {throw new MenuError(_error.message??'Ha ocurrido un error al tratar de buscar los menus.')});
 
-        const pagesWithMenu = menus.map(menu => {
+
+        if (access.super_admin === false) {
+            const uniquePageIds = new Set<string>();
+            const pagesWithMenu = menus.map((menu) => {
+                const page_id = menu.dataValues.page.dataValues.id;
+                if (access.super_admin && uniquePageIds.has(page_id)) return null;
+                uniquePageIds.add(page_id);
+                return {
+                    id: page_id,
+                    name: menu.dataValues.page.dataValues.name,
+                    ...(menu.dataValues.page.dataValues.external_url && { external_url: menu.dataValues.page.dataValues.external_url }),
+                    updatedAt: menu.dataValues.page.dataValues.updatedAt,
+                    createdAt: menu.dataValues.page.dataValues.createdAt
+                }
+            })
+            .filter((page) => page !== null)
+            return pagesWithMenu;
+        }
+
+        const pageWithMenuSuperAdmin = menus.map(menu => {
             return {
-                id: menu.dataValues.page_id,
-                name: menu.dataValues.page.dataValues.name,
-                ...(menu.dataValues.page.dataValues.external_url && { external_url: menu.dataValues.page.dataValues.external_url }),
-                updatedAt: menu.dataValues.page.dataValues.updatedAt,
-                createdAt: menu.dataValues.page.dataValues.createdAt
+                id: menu.dataValues.id,
+                name: menu.dataValues.name,
+                ...(menu.dataValues.external_url && { external_url: menu.dataValues.external_url }),
+                updatedAt: menu.dataValues.updatedAt,
+                createdAt: menu.dataValues.createdAt
             }
         });
-        return pagesWithMenu;
+        return pageWithMenuSuperAdmin;
     } catch (error) {
         if (error instanceof Error && error.message) throw new MenuError(error.message);
         else throw new Error('Ha ocurrido un error al buscar la pagina.');

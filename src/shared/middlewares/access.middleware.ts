b5a16fbcc44ae2cbbd2d02ead4e98f91a033d11e
Joseph Infante <josephinfante.y@gmail.com>
2023-12-12 00:27:10 -0500

    updating access middleware

--- src/shared/middlewares/access.middleware.ts
+++ src/shared/middlewares/access.middleware.ts
@@ -8,7 +8,7 @@ const secretKey: Uint8Array = new TextEncoder().encode(JWT_SECRET);
 
 type PermissionType = 'create' | 'read' | 'update' | 'delete';
 
-async function verifyAccess(component_name: string, permissionType: PermissionType, req: Request, res: Response, next: NextFunction) {
+async function verifyAccess(components: string[], permissionType: PermissionType, req: Request, res: Response, next: NextFunction) {
     try {
         const token = req.headers['x-user-token'] as string;
         if (!token) {
@@ -26,9 +26,65 @@ async function verifyAccess(component_name: string, permissionType: PermissionTy
 
         let permissions;
         let component_permission;
+        if (user.super_admin) {
+            // If user is super admin, allow the request
+            res.locals.role_id = user.role_id;
+            res.locals.access = {
+                user_id: payload.id,
+                user_name: `${user.first_name} ${user.last_name}`,
+                super_admin: user.super_admin,
+                role_name: 'super admin',
+                permission: {
+                    create: true,
+                    read: true,
+                    update: true,
+                    delete: true,
+                    read_all: true,
+                    read_deleted: true,
+                },
+            };
+            next();
+            return;
+        }
         if (user.role_id !== null) {
-            permissions = await GetPermissionsByRoleId(user.role_id);
-            component_permission = permissions.find(permission => permission.name === component_name);
+            for (const component_name of components) {
+                if (component_name === 'menu') {
+                    component_permission = {
+                        permissions: {
+                            create: false,
+                            read: true,
+                            update: false,
+                            delete: false,
+                            read_all: false,
+                            read_deleted: false,
+                        }
+                    }
+                } else {
+                    permissions = await GetPermissionsByRoleId(user.role_id);
+                    component_permission = permissions.find(permission => permission.name === component_name);
+                }
+
+                if (component_permission && component_permission.permissions[permissionType] === true) {
+                    // If permission is granted for any component, allow the request
+                    res.locals.role_id = user.role_id;
+                    res.locals.access = {
+                        user_id: payload.id,
+                        user_name: `${user.first_name} ${user.last_name}`,
+                        super_admin: user.super_admin,
+                        role_name: payload.role ?? '',
+                        permission: {
+                            create: component_permission?.permissions.create,
+                            read: component_permission?.permissions.read,
+                            update: component_permission?.permissions.update,
+                            delete: component_permission?.permissions.delete,
+                            read_all: component_permission?.permissions.read_all,
+                            read_deleted: component_permission?.permissions.read_deleted,
+                        },
+                    };
+                    next();
+                    return;
+                }
+            }
         }
 
         if (payload.exp && typeof payload.exp === 'number' && payload.exp < Date.now() / 1000) {
@@ -36,60 +92,35 @@ async function verifyAccess(component_name: string, permissionType: PermissionTy
             return;
         }
 
-        if (!component_permission && user.super_admin !== true) {
-            res.status(401).json({ error: "Acceso denegado. El usuario no tiene ningun permiso." });
-            return;
-        }
-
-        if ((component_permission && component_permission.permissions[permissionType] !== true) && user.super_admin !== true) {
-            res.status(401).json({ error: `Access denied. User doesn't have permission to ${permissionType} opportunities.` });
-            return;
-        }
-
-        res.locals.role_id = user.role_id;
-        res.locals.access = {
-            user_id: payload.id,
-            user_name: `${user.first_name} ${user.last_name}`,
-            super_admin: user.super_admin,
-            role_name: payload.role??'',
-            permission: {
-                create: component_permission?.permissions.create,
-                read: component_permission?.permissions.read,
-                update: component_permission?.permissions.update,
-                delete: component_permission?.permissions.delete,
-                read_all: component_permission?.permissions.read_all,
-                read_deleted: component_permission?.permissions.read_deleted,
-            },
-        };
-
-        next();
+        // If no component has the required permission, send a 401 response
+        res.status(401).json({ error: "Access denied. User doesn't have permission." });
     } catch (error: any) {
         res.status(401).json({ error: error.message || 'Access denied. Invalid token.' });
     }
 }
 
 export class Access {
-    static canCreate(component_name: string) {
+    static canCreate(components: string[]) {
         return async (req: Request, res: Response, next: NextFunction) => {
-            await verifyAccess(component_name, 'create', req, res, next);
+            await verifyAccess(components, 'create', req, res, next);
         };
     }
 
-    static canRead(component_name: string) {
+    static canRead(components: string[]) {
         return async (req: Request, res: Response, next: NextFunction) => {
-            await verifyAccess(component_name, 'read', req, res, next);
+            await verifyAccess(components, 'read', req, res, next);
         };
     }
 
-    static canUpdate(component_name: string) {
+    static canUpdate(components: string[]) {
         return async (req: Request, res: Response, next: NextFunction) => {
-            await verifyAccess(component_name, 'update', req, res, next);
+            await verifyAccess(components, 'update', req, res, next);
         };
     }
 
-    static canDelete(component_name: string) {
+    static canDelete(components: string[]) {
         return async (req: Request, res: Response, next: NextFunction) => {
-            await verifyAccess(component_name, 'delete', req, res, next);
+            await verifyAccess(components, 'delete', req, res, next);
         };
     }
 }

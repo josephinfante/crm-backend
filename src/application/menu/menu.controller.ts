8dc89940ff18feb490d1b0272b15e308efc60782
Joseph Infante <josephinfante.y@gmail.com>
2023-12-12 00:39:29 -0500

    updating functions

--- src/application/menu/menu.controller.ts
+++ src/application/menu/menu.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
-import { FindAllPagesWithRoleId } from "../../infrastructure/menu/menu.dao";
+import { FindAllMenusWithRoleId } from "../../infrastructure/menu/menu.dao";
 import { MenuError } from "../../shared/errors";
 
 export class MenuController {
-    async findAllPagesByRoleId(_req: Request, res: Response) {
+    async FindAllMenusWithRoleId(_req: Request, res: Response) {
         try {
-            const pages = await FindAllPagesWithRoleId(res.locals.access, res.locals.role_id);
+            const pages = await FindAllMenusWithRoleId(res.locals.access, res.locals.role_id);
             res.status(200).json(pages);
         } catch (error) {
             if (error instanceof MenuError) res.status(400).json({error: error.message});
--- src/infrastructure/menu/menu.dao.ts
+++ src/infrastructure/menu/menu.dao.ts
@@ -43,55 +43,23 @@ export async function DeleteMenu(access:IAccessPermission, role_id: string) {
     }
 }
 
-export async function FindPageWithMenu(access:IAccessPermission, role_id: string): Promise<IRolePage[]> {
-    try {
-        const menus = access.super_admin === true ?
-            await MenuModel.findAll({ 
-                    where: { role_id: role_id },
-                    include: [{ model: PageModel, attributes: ['id', 'name', 'external_url', 'createdAt', 'updatedAt'] }]
-                })
-                .then(menus => menus)
-                .catch(_error => {throw new MenuError('Ha ocurrido un error al tratar de buscar los menus.')}) :
-            await MenuModel.findAll({ 
-                    where: { role_id: role_id, user_id: access.user_id },
-                    include: [{ model: PageModel, attributes: ['id', 'name', 'external_url', 'createdAt', 'updatedAt'] }]
-                })
-                .then(menus => menus)
-                .catch(_error => {throw new MenuError('Ha ocurrido un error al tratar de buscar los menus.')});
-
-        const pagesWithMenu = menus.map(menu => {
-            return {
-                id: menu.dataValues.page_id,
-                name: menu.dataValues.page.dataValues.name,
-                ...(menu.dataValues.page.dataValues.external_url && { external_url: menu.dataValues.page.dataValues.external_url }),
-                updatedAt: menu.dataValues.page.dataValues.updatedAt,
-                createdAt: menu.dataValues.page.dataValues.createdAt
-            }
-        });
-        return pagesWithMenu;
-    } catch (error) {
-        if (error instanceof Error && error.message) throw new MenuError(error.message);
-        else throw new Error('Ha ocurrido un error al buscar la pagina.');
-    }
-}
-
-export async function FindAllPagesWithRoleId( access: IAccessPermission, role_id: string ): Promise<IRolePage[]> {
+export async function FindAllMenusWithRoleId( access: IAccessPermission, role_id: string ): Promise<IRolePage[]> {
     try {
         const menus = access.super_admin === true ?
             await PageModel.findAll({
                     where: { deleted: false, hidden: false },
                 })
                 .then(pages => pages)
-                .catch(_error => {throw new MenuError(_error.message??'Ha ocurrido un error al tratar de buscar las páginas.')}) :
+                .catch(_error => {throw new MenuError('Ha ocurrido un error al tratar de buscar las páginas.')}) :
             await MenuModel.findAll({ 
                     where: { role_id: role_id },
-                    include: [{ model: PageModel, attributes: ['id', 'name', 'external_url', 'createdAt', 'updatedAt'], where: { deleted: false, hidden: false } }]
+                    include: [{ model: PageModel, where: { deleted: false, hidden: false } }]
                 })
                 .then(menus => menus)
-                .catch(_error => {throw new MenuError(_error.message??'Ha ocurrido un error al tratar de buscar los menus.')});
+                .catch(_error => {throw new MenuError('Ha ocurrido un error al tratar de buscar los menus.')});
 
 
-        if (access.super_admin === false) {
+        if (access.super_admin !== true) {
             const uniquePageIds = new Set<string>();
             const pagesWithMenu = menus.map((menu) => {
                 const page_id = menu.dataValues.page.dataValues.id;
@@ -119,6 +87,29 @@ export async function FindAllPagesWithRoleId( access: IAccessPermission, role_id
             }
         });
         return pageWithMenuSuperAdmin;
+    } catch (error) {
+        if (error instanceof Error && error.message) throw new MenuError(error.message);
+        else throw new Error('Ha ocurrido un error al buscar la pagina.');
+    }
+}
+
+export async function FindAllPagesWithRoleId(role_id: string ): Promise<IRolePage[]> {
+    try {
+        const menus = await MenuModel.findAll({ 
+                where: { role_id: role_id },
+                include: [{ model: PageModel, where: { deleted: false, hidden: false } }]
+            })
+            .then(menus => menus)
+            .catch(_error => {throw new MenuError('Ha ocurrido un error al tratar de buscar los menus.')});
+
+        return menus.map(menu => {
+            return {
+                id: menu.dataValues.id,
+                name: menu.dataValues.name,
+                ...(menu.dataValues.external_url && { external_url: menu.dataValues.external_url }),
+                updatedAt: menu.dataValues.updatedAt,
+                createdAt: menu.dataValues.createdAt
+        }});
     } catch (error) {
         if (error instanceof Error && error.message) throw new MenuError(error.message);
         else throw new Error('Ha ocurrido un error al buscar la pagina.');
